37466dd518fb452d01cad0703d6fd542c9f9d33b
Malay Nayak <malay.kanta.nayak@gmail.com>
2019-09-03 19:56:36 +0530

    REST API Customisations

--- app/wp-content/themes/university-theme/functions.php
+++ app/wp-content/themes/university-theme/functions.php
@@ -1,5 +1,17 @@
 <?php
 
+require get_theme_file_path('/inc/search-route.php');
+
+// Customize rest api.
+function university_custom_rest() {
+    register_rest_field('post', 'authorName', array(
+        'get_callback' => function() {
+            return get_the_author();
+        }
+    ));
+}
+add_action('rest_api_init',  'university_custom_rest');
+
 // Dynamic banner.
 function pageBanner($args= NULL) { 
     if (!$args['title']) {
