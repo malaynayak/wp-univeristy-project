714df182f28533570390d1751d9b47b484b2d729
Malay Nayak <malay.kanta.nayak@gmail.com>
2019-09-03 19:55:55 +0530

    Custom REST Router

---
+++ app/wp-content/themes/university-theme/inc/search-route.php
@@ -0,0 +1,73 @@
+<?php 
+
+// Custom rest route.
+add_action('rest_api_init', 'universityRegisterSearch');
+
+function universityRegisterSearch() {
+    register_rest_route(
+        'university/v1',
+        'search',
+        [
+            'methods' => WP_REST_Server::READABLE,
+            'callback' => 'universitySearchResults'
+        ]
+    );
+}
+
+function universitySearchResults($data) {
+    $mainQuery = new WP_Query([
+        'post_type' => [
+            'page',
+            'post',
+            'professor',
+            'program',
+            'campus',
+            'event'
+        ],
+        's' => sanitize_text_field($data['term'])
+    ]);
+    $results = [
+        'generalInfo' => [],
+        'professors' => [],
+        'programs' => [],
+        'events' => [],
+        'campuses' => []
+    ];
+
+    while($mainQuery->have_posts()) {
+        $mainQuery->the_post();
+
+        if (get_post_type() == 'post' || get_post_type() == 'page') {
+            array_push($results['generalInfo'], [
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ]);
+        }
+
+        if (get_post_type() == 'professor') {
+            array_push($results['professors'], [
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ]);
+        }
+        if (get_post_type() == 'program') {
+            array_push($results['programs'], [
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ]);
+        }
+        if (get_post_type() == 'event') {
+            array_push($results['events'], [
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ]);
+        }
+        if (get_post_type() == 'campus') {
+            array_push($results['campuses'], [
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ]);
+        }
+    }
+    return $results;
+}
