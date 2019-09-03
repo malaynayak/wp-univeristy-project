77e63aedab78938b24c5036f546b3cc22097129f
Malay <malay.kanta.nayak@gmail.com>
2019-09-04 00:15:32 +0530

    Search page templating.

--- app/wp-content/themes/university-theme/inc/search-route.php
+++ app/wp-content/themes/university-theme/inc/search-route.php
@@ -40,14 +40,17 @@ function universitySearchResults($data) {
         if (get_post_type() == 'post' || get_post_type() == 'page') {
             array_push($results['generalInfo'], [
                 'title' => get_the_title(),
-                'permalink' => get_the_permalink()
+                'permalink' => get_the_permalink(),
+                'postType' => get_post_type(),
+                'authorName' => get_the_author()
             ]);
         }
 
         if (get_post_type() == 'professor') {
             array_push($results['professors'], [
                 'title' => get_the_title(),
-                'permalink' => get_the_permalink()
+                'permalink' => get_the_permalink(),
+                'image' => get_the_post_thumbnail_url(0,'professorLandscape')
             ]);
         }
         if (get_post_type() == 'program') {
@@ -57,9 +60,13 @@ function universitySearchResults($data) {
             ]);
         }
         if (get_post_type() == 'event') {
+            $eventDate = new DateTime(get_field('event_date'));
             array_push($results['events'], [
                 'title' => get_the_title(),
-                'permalink' => get_the_permalink()
+                'permalink' => get_the_permalink(),
+                'month' => $eventDate->format('M'),
+                'day' => $eventDate->format('d'),
+                'description' => (has_excerpt()) ? get_the_excerpt() : wp_trim_words(get_the_content(), 18)
             ]);
         }
         if (get_post_type() == 'campus') {
