b68c8f2dc6dfae858ca1cc86c3008fbb6e78b413
Malay Nayak <malay.kanta.nayak@gmail.com>
2019-09-06 20:47:17 +0530

    Merged User notes

---
+++ app/wp-content/themes/university-theme/js/modules/MyNote.js
@@ -0,0 +1,126 @@
+import $ from 'jquery';
+
+class MyNotes {
+    constructor() {
+        this.events();
+    }
+    // Handle events.
+    events() {
+        $('#my-notes').on('click', '.delete-note', this.deleteNote);
+        $('#my-notes').on('click', '.edit-note', this.editNote.bind(this));
+        $('#my-notes').on('click', '.update-note', this.updateNote.bind(this));
+        $('.submit-note').on('click', this.createNote.bind(this));
+    }
+    // Delete a note.
+    deleteNote(event) {
+        var thisNote = $(event.target).parents('li');
+        $.ajax({
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+            },
+            url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
+            type: 'DELETE',
+            success: (response) => {
+                thisNote.slideUp();
+                if(response.userNoteCount < 5) {
+                    $('.note-limit-message').removeClass('active');
+                }
+                console.log(response);
+            },
+            error: (error) => {
+                console.log(error);
+            }
+        })
+    }
+    // Update a note.
+    updateNote(event) {
+        var thisNote = $(event.target).parents('li');
+
+        var updatedNote = {
+            'title': thisNote.find('.note-title-field').val(),
+            'content': thisNote.find('.note-body-field').val()
+        };
+
+        $.ajax({
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+            },
+            url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
+            type: 'POST',
+            data: updatedNote,
+            success: (response) => {
+                this.makeNoteReadOnly(thisNote);
+                console.log(response);
+            },
+            error: (error) => {
+                console.log(error);
+            }
+        })
+    }
+    // Create a note.
+    createNote(event) {
+        var newNote = {
+            'title': $('.new-note-title').val(),
+            'content': $('.new-note-body').val(),
+            'status': 'private',
+        };
+
+        $.ajax({
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+            },
+            url: universityData.root_url + '/wp-json/wp/v2/note/',
+            type: 'POST',
+            data: newNote,
+            success: (response) => {
+                $('.new-note-title, .new-note-body').val('');
+                $(`
+                    <li readonly data-id="${response.id}">
+                        <input class="note-title-field" type="text" value="${response.title.raw}">
+                        <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>
+                        <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
+                        <textarea readonly class="note-body-field" name="note_body" id="note_body">${response.content.raw}</textarea>
+                        <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i> Save</span>
+                    </li>
+                `).prependTo('#my-notes').hide().slideDown();
+                console.log(response);
+            },
+            error: (error) => {
+                if (error.responseText == 'Note limit reached') {
+                    $('.note-limit-message').addClass('active');
+                }
+                console.log(error);
+            }
+        })
+    }
+
+    // Edit note event handling
+    editNote(event) {
+        var thisNote = $(event.target).parents('li');
+        if (thisNote.data('state') == 'editable') {
+            this.makeNoteReadOnly(thisNote);
+        } else {
+            this.makeNoteEditable(thisNote);
+        }
+    }
+
+    makeNoteEditable(thisNote) {
+        thisNote.find('.edit-note')
+            .html('<i class="fa fa-times" aria-hidden="true"></i> Cancel');
+        thisNote.find('.note-title-field, .note-body-field')
+            .removeAttr('readonly').addClass("note-active-field");
+        thisNote.find('.update-note').addClass('update-note--visible');
+        thisNote.data('state', 'editable');
+    }
+
+    makeNoteReadOnly(thisNote) {
+        thisNote.find('.edit-note')
+            .html('<i class="fa fa-pencil" aria-hidden="true"></i> Edit');
+        thisNote.find('.note-title-field, .note-body-field')
+            .attr('readonly', 'readonly').removeClass("note-active-field");
+        thisNote.find('.update-note').removeClass('update-note--visible');
+        thisNote.data('state', 'cancel');
+    }
+}
+
+export default MyNotes;
